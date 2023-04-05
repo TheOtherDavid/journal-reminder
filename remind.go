e42ce4c8ddabdc817fa0bb17a388024ae8ac3a17
David <davidhand1988@gmail.com>
2023-04-05 00:24:41 -0400

    Fix issues with weird fields being nil.

--- remind.go
+++ remind.go
@@ -19,105 +19,96 @@ import (
 )
 
 func Remind(documentIds []string) (err error) {
-	// Read credentials from file
-	creds, err := ioutil.ReadFile("../../credentials.json")
-	if err != nil {
-		log.Fatalf("Unable to read client secret file: %v", err)
-	}
-
-	// Set up the OAuth2 config
-	config, err := google.ConfigFromJSON(creds, docs.DocumentsScope)
-	if err != nil {
-		log.Fatalf("Unable to parse client secret file to config: %v", err)
-	}
-
-	// Read or obtain the token
-	token, err := getToken(config)
-	if err != nil {
-		log.Fatalf("Unable to get token: %v", err)
-	}
-
-	// Set up the token source
-	ts := config.TokenSource(context.Background(), token)
-
 	// Create a new Docs service client with the token source
-	client, err := docs.NewService(context.Background(), option.WithHTTPClient(oauth2.NewClient(context.Background(), ts)))
+	client, err := GetGoogleClient()
 	if err != nil {
 		log.Fatalf("Unable to create client: %v", err)
 	}
 
-	// Specify the document ID to read
-	documentID := documentIds[0]
-
-	// Call the API to retrieve the document content
-	doc, err := client.Documents.Get(documentID).Do()
-	if err != nil {
-		log.Fatalf("Unable to retrieve document: %v", err)
-	}
-
-	// Print the document content
-	fmt.Printf("The document title is: %s\n", doc.Title)
-	//fmt.Printf("The document content is: %s\n", doc.Body.Content)
-
-	//Maybe this could be pulled into a "extract journal entry" function
-
-	//Take the target year from the journal title
-	targetYear := doc.Title[8:]
-	targetYearInt, _ := strconv.Atoi(targetYear)
-	currentTime := time.Now()
-	targetDate := time.Date(targetYearInt, currentTime.Month(), currentTime.Day(), 0, 0, 0, 0, time.UTC)
-	//currentDayString := currentTime.Format("1/2/2006")
-
-	append := false
 	message := "\n"
 
-	for _, contentItem := range doc.Body.Content {
-		//Parse file, specifically try to look for headers.
-		//If the current content list's text is type Heading 4, then we need to append the NEXT several paragraphs to a message.
-		//Then we need to do that UNTIL there's another Heading.
-		if contentItem.Paragraph != nil && contentItem.Paragraph.ParagraphStyle.NamedStyleType == "HEADING_4" {
-			if append == true {
-				//If we encounter a header when in append mode, we turn off append mode
-				append = false
+	for _, documentID := range documentIds {
+		// Call the API to retrieve the document content
+		doc, err := client.Documents.Get(documentID).Do()
+		if err != nil {
+			log.Fatalf("Unable to retrieve document: %v", err)
+		}
+
+		// Print the document content
+		fmt.Printf("The document title is: %s\n", doc.Title)
+		//fmt.Printf("The document content is: %s\n", doc.Body.Content)
+
+		//Maybe this could be pulled into a "extract journal entry" function
+
+		//Take the target year from the journal title
+		targetYear := doc.Title[8:]
+		targetYearInt, _ := strconv.Atoi(targetYear)
+		currentTime := time.Now()
+		targetDate := time.Date(targetYearInt, currentTime.Month(), currentTime.Day(), 0, 0, 0, 0, time.UTC)
+		//currentDayString := currentTime.Format("1/2/2006")
+
+		append := false
+		done := false
+
+		for _, contentItem := range doc.Body.Content {
+			//Parse file, specifically try to look for headers.
+			//If the current content list's text is type Heading 4, then we need to append the NEXT several paragraphs to a message.
+			//Then we need to do that UNTIL there's another Heading.
+			if done {
 				break
 			}
-			if append == false {
-				//If we aren't in append mode, check which date format to use
-				element := contentItem.Paragraph.Elements[0]
-				if element != nil {
-					textRunContent := element.TextRun.Content
-					if textRunContent != "" && textRunContent != "\n" {
-						//check the date on the entry
-						dateString := strings.Split(textRunContent, ",")[0]
-						headingDate := time.Time{}
-						layout := ""
+			if contentItem.Paragraph != nil && contentItem.Paragraph.ParagraphStyle.NamedStyleType == "HEADING_4" {
+				if append == true {
+					//If we encounter a header when in append mode, we turn off append mode
+					append = false
+					done = true
+					break
+				}
+				if append == false {
+					//If we aren't in append mode, check which date format to use
+					element := contentItem.Paragraph.Elements[0]
+					if element != nil {
+						if element.TextRun != nil {
+							textRunContent := element.TextRun.Content
+							if textRunContent != "" && textRunContent != "\n" {
+								//check the date on the entry
+								dateString := strings.Split(textRunContent, ",")[0]
+								headingDate := time.Time{}
+								layout := ""
 
-						layout = "1/2/2006"
-						//Convert to date
-						headingDate, err := time.Parse(layout, dateString)
-						if err != nil {
-							fmt.Println("English date parse failed, trying European.")
-							layout = "2/1/2006"
-							headingDate, err = time.Parse(layout, dateString)
-							if err != nil {
-								fmt.Println("European date parse failed too. What's wrong?")
-								return err
+								layout = "1/2/2006"
+								//Convert to date
+								headingDate, err := time.Parse(layout, dateString)
+								if err != nil {
+									fmt.Println("English date parse failed, trying European.")
+									layout = "2/1/2006"
+									headingDate, err = time.Parse(layout, dateString)
+									if err != nil {
+										fmt.Println("European date parse failed too. What's wrong?")
+										return err
+									}
+								}
+								//Now we need to compare the target (current) date to the heading date, to see if this is today's entry.
+								if headingDate.Equal(targetDate) {
+									//If the heading is the current day, enter append mode
+									append = true
+								}
 							}
-						}
-						//Now we need to compare the target (current) date to the heading date, to see if this is today's entry.
-						if headingDate.Equal(targetDate) {
-							//If the heading is the current day, enter append mode
-							append = true
 						}
 					}
 				}
 			}
-		}
-		//If in append mode, append message
-		if append == true {
-			for _, element := range contentItem.Paragraph.Elements {
-				if element.PageBreak == nil {
-					message = message + element.TextRun.Content
+			//If in append mode, append message
+			if append == true {
+				for _, element := range contentItem.Paragraph.Elements {
+					if element.PageBreak == nil {
+						message = message + element.TextRun.Content
+					} else {
+						fmt.Println("Page break detected.")
+						append = false
+						done = true
+						break
+					}
 				}
 			}
 		}
@@ -129,7 +120,7 @@ func Remind(documentIds []string) (err error) {
 	senderEmail := os.Getenv("SENDER_EMAIL")
 	senderPassword := os.Getenv("SENDER_PASSWORD")
 	recipientEmail := os.Getenv("RECIPIENT_EMAIL")
-	err = SendEmail(recipientEmail, senderEmail, senderPassword, targetDate, message)
+	err = SendEmail(recipientEmail, senderEmail, senderPassword, message)
 	if err != nil {
 		fmt.Println(err)
 		return err
@@ -222,13 +213,13 @@ type GoogleDocumentTextRun struct {
 	Content string `json:"content"`
 }
 
-func SendEmail(recipientAddress string, senderAddress string, senderPassword string, entryDate time.Time, body string) error {
+func SendEmail(recipientAddress string, senderAddress string, senderPassword string, body string) error {
 	from := senderAddress
 	password := senderPassword
 	to := []string{recipientAddress}
 	smtpHost := "smtp.gmail.com"
 	smtpPort := "587"
-	dateString := entryDate.Format("2006-01-02")
+	dateString := time.Now().Format("2006-01-02")
 	subject := "Journal Reminder for " + dateString
 
 	headers := make(map[string]string)
@@ -253,6 +244,30 @@ func SendEmail(recipientAddress string, senderAddress string, senderPassword str
 		return err
 	}
 	return nil
+}
+
+func GetGoogleClient() (*docs.Service, error) {
+	// Read credentials from file
+	creds, err := ioutil.ReadFile("../../credentials.json")
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+
+	// Set up the OAuth2 config
+	config, err := google.ConfigFromJSON(creds, docs.DocumentsScope)
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+
+	// Read or obtain the token
+	token, err := getToken(config)
+	if err != nil {
+		log.Fatalf("Unable to get token: %v", err)
+	}
+
+	// Set up the token source
+	ts := config.TokenSource(context.Background(), token)
+	return docs.NewService(context.Background(), option.WithHTTPClient(oauth2.NewClient(context.Background(), ts)))
 }
 
 func RefreshGoogleAuth() (authCode string) {
